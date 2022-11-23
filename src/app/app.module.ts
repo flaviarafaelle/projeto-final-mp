f9a6f001bfac8ccea95ac61bf1f0ca4dbf74394e
flavinha <flaviarlimasantos@gmail.com>
2022-11-23 19:09:10 -0300

    fix: hero page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './components/services/services.component';
+import { BookComponent } from './components/book/book.component';
+import { DisabledComponent } from './components/disabled/disabled.component';
+import { HeartComponent } from './components/heart/heart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    BookComponent,
+    DisabledComponent,
+    HeartComponent
   ],
   imports: [
     BrowserModule,
