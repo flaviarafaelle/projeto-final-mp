a9b8a92e79736036725fadb4d0264518b8017672
flavinha <flaviarlimasantos@gmail.com>
2022-11-23 21:51:12 -0300

    feat: services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServicesComponent } from './components/services/services.component';
-import { BookComponent } from './components/book/book.component';
-import { DisabledComponent } from './components/disabled/disabled.component';
-import { HeartComponent } from './components/heart/heart.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    ServicesComponent,
-    BookComponent,
-    DisabledComponent,
-    HeartComponent
+
   ],
   imports: [
     BrowserModule,
